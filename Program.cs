7e4d15a57c046107f72c7e81ec25167863493444
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-08-26 00:14:10 +0300

    выходной ли сегодня ?

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели");
+int a = int.Parse(Console.ReadLine());
+if ( a == 6 ) Console.WriteLine("Да");
+if ( a == 7 ) Console.WriteLine("Да");
+if ( a > 8 ) Console.WriteLine("Некорректное значение");
+if ( a <= 5) Console.WriteLine("Нет");
+
